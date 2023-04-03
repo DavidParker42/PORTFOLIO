ed0ae5d2c72f06c2b233d434f82516a8fdb7b516
David Parker <david.m.parker92@gmail.com>
2023-04-03 16:34:08 -0400

    added to app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Contact from './pages/contact';
 import Footer from './components/footer';
 import Work from "./pages/work";
 import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+// import { Routes, Route } from 'react-router-dom';
 import { Routes, Route, Navigate } from "react-router-dom";
 
 
@@ -22,7 +22,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Header />} />
       <Route path="/about" element={<Aboutme />} />
-      {/* <Route path='/contact' element={<Contact />}/> */}
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/work" element={<Work />} />
       {/* <Route path='/resume' element={<Resume />}/> */}
       <Route path="/PORTFOLIO" element={<Navigate replace to="/" />} />
     </Routes>
