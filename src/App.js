281b61676801341000a7e6399e138bf97f7c69ba
David Parker <david.m.parker92@gmail.com>
2023-04-03 21:30:52 -0400

    small change to contact

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ export default function PortfolioContainer() {
   // TODO: Add a comment describing the functionality of this method
   // this method is checking to see what the value of 1currentPage` is. Depending on the value of currentPage, we return the corresponding component to render
   const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
+    if (currentPage === 'Contact') {
+      return <Contact />;
     }
     if (currentPage === 'AboutMe') {
       return <AboutMe />;
